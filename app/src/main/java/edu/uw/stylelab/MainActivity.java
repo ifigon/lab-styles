04aa0c26afdc84fabc5e388ef92fb5b1ab87e4bc
Ian Figon <ifigon@outlook.com>
2018-10-17 14:43:04 -0700

    finished

--- app/src/main/java/edu/uw/stylelab/MainActivity.java
+++ app/src/main/java/edu/uw/stylelab/MainActivity.java
@@ -2,8 +2,9 @@ package edu.uw.stylelab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
